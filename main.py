6ae2c0d7bcec496df330eea555e05e0f02a7b169
HyagoerS <hyagossoficiall@gmail.com>
2025-05-22 20:00:05 +0000

    continuar

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 def q1():
+
+
     pass
 
 def q2():
-    pass
+    numeros = []
+
+    while True:
+        n = int(input("Digite um número: "))
+        if n < 0:
+            print("Número negativo")
+            break
+        numeros.append(n)
+    for n in numeros:
+        if n % 2 == 1:
+            print("Primo")
+        else:
+            print("Não")
+
 
 def q3():
     pass
@@ -14,4 +29,4 @@ def q5():
     pass
 
 if __name__=="__main__":
-    # teste sua questão manualmente aqui
+    q2()
